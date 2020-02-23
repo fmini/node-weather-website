ee936b525f557c0e591191985aaf90f479a1a584
Frank Minichiello <9028036+fmini@users.noreply.github.com>
2020-02-23 13:34:38 -0700

    Added day high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find provided location.', undefined)
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currentyly ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+      callback(undefined, `${body.daily.data[0].summary} It is currentyly ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Today's high is expected to be ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}`);
     }
   })
 }
